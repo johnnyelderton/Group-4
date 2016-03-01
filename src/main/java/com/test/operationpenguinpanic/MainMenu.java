13c282967477758d626014b86c497377c152f6ea
Evan-Downard <emdownard@gmail.com>
2016-03-01 09:54:19 -0600

    My own xml, java, and drawable files right now
    The drawable items might be broken. More will be uploaded tonight.

---
+++ src/main/java/com/test/operationpenguinpanic/MainMenu.java
@@ -0,0 +1,43 @@
+package com.test.operationpenguinpanic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void sendGame(View view){
+        //Intent intent = new Intent(this, DisplayGame.class);
+        setContentView(R.layout.game_select);
+    }
+    public void sendCustom(View view){
+
+    }
+    public void sendOption(View view){
+
+    }
+    public void sendScore(View view){
+
+    }
+
+    public void sendRacing(View view){
+        setContentView(new GamePanel(this));
+    }
+    public void sendMarathon(View view){
+
+    }
+    public void sendAsteroids(View view){
+
+    }
+}
+
